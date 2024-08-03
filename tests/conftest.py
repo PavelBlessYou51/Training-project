9f89194b2fd8ebd13c5fe7ee085e3e73c7869417
PavelBlessYou51 <prokoshevpavel@mail.ru>
2024-08-03 18:17:05 +0300

    CheckBox tests is complite

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,14 +4,15 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.webdriver import WebDriver
 from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture(scope='module')
-def get_driver():
+def get_driver() -> WebDriver:
     options = Options()
     options.add_experimental_option("detach", True)
     driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
     driver.maximize_window()
     yield driver
-    driver.quit()
+    # driver.quit()
--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,6 +1,6 @@
 import allure
 
-from pages.elements_page import TextBoxPage
+from pages.elements_page import TextBoxPage, CheckBoxPage
 
 
 @allure.suite('Test of elements')
@@ -14,3 +14,16 @@ class TestElements:
             input_data = text_box_page.fill_form()
             output_data = text_box_page.get_output_data()
             assert input_data == output_data, "The data does not match"
+
+    @allure.feature('Test of CheckBox')
+    class TestCheckBox:
+
+        @allure.title('CheckBox Test')
+        def test_check_box(self, get_driver):
+            check_box_page = CheckBoxPage(get_driver, 'https://demoqa.com/checkbox')
+            check_box_page.open()
+            check_box_page.expend_all_boxes()
+            check_box_page.select_random_boxes()
+            checked_boxes = check_box_page.collect_titles_of_checked_boxes()
+            output = check_box_page.get_output()
+            assert checked_boxes == output, "The checked boxes do not match"
